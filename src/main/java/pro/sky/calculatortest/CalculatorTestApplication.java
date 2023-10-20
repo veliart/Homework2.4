2cc0382d88feb32852778eda1d0b871c30582aa4
Artur Vakhitov <arthurvakhitov@gmail.com>
2023-10-20 20:11:26 +0300

    Start homework 2.4

---
+++ src/main/java/pro/sky/calculatortest/CalculatorTestApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/calculatortest/CalculatorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
